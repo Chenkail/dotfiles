ba708ab60a8b0cdf7e8024abeba98f756c47720f
chenkail <40770208+Chenkail@users.noreply.github.com>
2020-12-21 19:04:23 -0800

    Add zsh files

---
+++ zsh/README.md
@@ -0,0 +1,11 @@
+# zsh config
+
+Steps:
+
+- Install zsh
+- Install prezto
+- Install zplug
+- Change .zpreztorc
+- Change .zshrc
+- Install p10k
+- Install thefuck
---
+++ zsh/zsh_setup.sh
@@ -0,0 +1,16 @@
+#! /bin/zsh
+
+# Install prezto
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
+# zplug
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+
+
+
+fuck
+fuck
