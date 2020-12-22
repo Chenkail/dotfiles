20fb8dfa8054737a666ed26e10f5f5d23e757a2f
chenkail <40770208+Chenkail@users.noreply.github.com>
2020-12-21 19:33:05 -0800

    Update zsh setup script

--- zsh/zsh_setup.sh
+++ zsh/zsh_setup.sh
@@ -1,5 +1,11 @@
 #! /bin/zsh
 
+# Home directory
+cd ~
+
+# Repository folder for zsh config files
+DOTFILES='https://raw.githubusercontent.com/Chenkail/dotfiles/main/zsh'
+
 # Install prezto
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 setopt EXTENDED_GLOB
@@ -10,7 +16,15 @@ done
 # zplug
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 
+curl -O ${DOTFILES}/.zpretzorc
+curl -O ${DOTFILES}/.zshrc
 
-
+# Set up thefuck
 fuck
 fuck
+
+# Reload zsh
+exec zsh
+
+# Run p10k setup
+p10k configure
