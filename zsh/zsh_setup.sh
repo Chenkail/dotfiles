d0317162845f47cb4d978b977b77770c8a46bae9
chenkail <40770208+Chenkail@users.noreply.github.com>
2020-12-22 16:12:42 -0800

    Check if zplug and zprezto are already installed

--- zsh/zsh_setup.sh
+++ zsh/zsh_setup.sh
@@ -7,17 +7,21 @@ cd ~
 DOTFILES='https://raw.githubusercontent.com/Chenkail/dotfiles/main/zsh'
 
 # Install prezto
-echo "Installing prezto:"
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-done
-echo
+if [ -d ".zprezto" ]; then
+  echo "Installing prezto:"
+  git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+  setopt EXTENDED_GLOB
+  for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+    ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+  done
+  echo
+fi
 
 # zplug
-echo "Installing zplug:"
-curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+if [ -d ".zplug" ]; then
+  echo "Installing zplug:"
+  curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+fi
 
 # Config files
 echo "Copying config files:"
