d4bdbbc7e9080c3a157fe3987e513a232d9af2d5
chenkail <40770208+Chenkail@users.noreply.github.com>
2020-12-22 16:08:54 -0800

    Add progress messages and fix prompts

--- zsh/zsh_setup.sh
+++ zsh/zsh_setup.sh
@@ -7,26 +7,30 @@ cd ~
 DOTFILES='https://raw.githubusercontent.com/Chenkail/dotfiles/main/zsh'
 
 # Install prezto
+echo "Installing prezto:"
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
+echo
 
 # zplug
+echo "Installing zplug:"
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 
-
 # Config files
+echo "Copying config files:"
 curl ${DOTFILES}/.zpretzorc > zpretzorc.new
 curl ${DOTFILES}/.zshrc > zshrc.new
 
 mv zpretzorc.new "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/zpretzorc
 mv zshrc.new "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/zshrc
+echo
 
 # Set up thefuck
 read name\?"Configure thefuck? "
-if [[ ! $REPLY =~ ^[Yy]$ ]]
+if [[ $REPLY =~ ^[Yy]$ ]]
 then
   fuck
   fuck
@@ -34,10 +38,11 @@ fi
 
 # Run p10k setup
 read name\?"Configure p10k? "
-if [[ ! $REPLY =~ ^[Yy]$ ]]
+if [[ $REPLY =~ ^[Yy]$ ]]
 then
   p10k configure
 fi
 
+echo "Reloading zsh!"
 # Reload zsh
 exec zsh
